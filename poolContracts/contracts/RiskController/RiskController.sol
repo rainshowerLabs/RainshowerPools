57c1bf541735f8e1150afcd41e27a3c3c00b669b
Vukasin Gostovic <vukasin@gostovic.me>
2023-05-12 19:22:18 +0100

    add risk controller

---
+++ poolContracts/contracts/RiskController/RiskController.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.8.0;
+
+/**
+ * The RiskController asseses risk kinda
+ */
+contract RiskController {
+	function assessPair (address _uniswapPair) returns(bool) external view {
+		return true;
+	}
+}
+
