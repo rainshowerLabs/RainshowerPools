fa8ab23129387e435aa43eba3f65d81d3000dffd
Vukasin Gostovic <vukasin@gostovic.me>
2023-05-12 23:25:57 +0100

    add data adapters

---
+++ poolContracts/contracts/DataAdapters/TotalReturnSwapAdapter.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: 3BSD
+pragma solidity ^0.8.0;    
+
+contract TotalReturnSwapDataAdapter {
+    struct params {
+        address marginToken;
+        address quoteToken;
+        address oracleAddress;
+        uint32 period;
+        // Expressed as a minimum of how much the taker deposit can be 
+        // swaped to the quote token.
+        uint80 maintanenceMargin;
+        uint56 initialDepositTimestamp;
+        uint88 hourlyTakerFee;
+        uint256 entryPrice;
+        uint128 makerDeposit;
+        uint128 takerDeposit;
+    }
+
+	function decode (bytes memory _data) returns (params memory trs) public {
+		trs = abi.decode(_data, (swapData));
+		return trs;
+	}	
+}
---
+++ poolContracts/contracts/DataAdapters/UniswapAdapter.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: 3BSD
+pragma solidity ^0.8.0;    
+
+contract UniswapDataAdapter {
+	struct swapData {
+	    address quoteToken;
+	    uint128 quoteAmount;
+	    address baseToken;
+	    address oracleAddress;
+	    uint24 poolFee;
+	    uint32 period;
+	    uint80 maintanenceMargin;
+	    address swapRouterAddress;
+	    address pool;
+	}
+
+	function decode (bytes memory _data) returns (swapData memory swap) public {
+		swap = abi.decode(_data, (swapData));
+		return swap;
+	}	
+}
