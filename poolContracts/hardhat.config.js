77e14dc37c80d1f3742d1dce4e6563c4ce8113d3
Antonio Furtado <antoniordf@hotmail.com>
2023-05-13 15:51:16 +0100

    Wrote deploy script, deployed contracts and used deployed address to start bringing in the contract instance into components

--- poolContracts/hardhat.config.js
+++ poolContracts/hardhat.config.js
@@ -27,7 +27,9 @@ module.exports = {
     local: {
       url: "http://127.0.0.1:8545/",
       accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [""],
+        process.env.PRIVATE_KEY !== undefined
+          ? [process.env.PRIVATE_KEY]
+          : [""],
     },
   },
   gasReporter: {
