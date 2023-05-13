955062da0ee9452b61e9860ebe4dc40d00ca87ac
halo3mic <46010359+halo3mic@users.noreply.github.com>
2023-05-13 03:53:39 +0100

    update uni-data-adapter

--- poolContracts/contracts/DataAdapters/UniswapAdapter.sol
+++ poolContracts/contracts/DataAdapters/UniswapAdapter.sol
@@ -1,21 +1,16 @@
 // SPDX-License-Identifier: 3BSD
 pragma solidity ^0.8.0;
 
+
 contract UniswapDataAdapter {
-	struct swapData {
-	    address quoteToken; // this 
-	    uint128 quoteAmount;
-	    address baseToken;
-	    address oracleAddress;
-	    uint24 poolFee;
-	    uint32 period;
-	    uint80 maintanenceMargin;
-	    address swapRouterAddress;
-	    address pool;
+
+	function decode(
+		bytes memory _data
+	) public pure returns (address quoteToken, uint256 quoteAmount) {
+		assembly {
+            quoteToken := mload(add(_data, 0x20))
+            quoteAmount := mload(add(_data, 0x40))
+        }
 	}
 
-	function decode (bytes memory _data) public returns (address _quoteToken, uint128 _quoteAmount) {
-		swap = abi.decode(_data, (swapData));
-		return swap;
-	}	
 }
