006fdfecfe505ef597f62d65eb30ef21913f5fb2
Antonio Furtado <antoniordf@hotmail.com>
2023-05-13 12:24:50 +0100

    Implemented ethers

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./index.css";
 import Navbar from "./components/Navbar";
 import MainContent from "./components/MainContent";
+import RainshowerContract from "./RainshowerContract.json";
 
 // Import the ethers library
-// const { ethers } = window;
-
-// Connect to an Ethereum provider
-// const provider = new ethers.providers.Web3Provider(window.ethereum);
-
-// Get the signer (the user's account)
-// const signer = provider.getSigner();
-
-// The address of your smart contract
-// const contractAddress = '0x...';
-
-// The ABI (Application Binary Interface) of your smart contract
-// const contractABI = [...];
-
-// Create a contract instance
-// const contract = new ethers.Contract(contractAddress, contractABI, signer);
+import { ethers } from "ethers";
 
 function App() {
+  const [provider, setProvider] = useState(null);
+  const [signer, setSigner] = useState(null);
+  const [contract, setContract] = useState(null);
+
+  useEffect(() => {
+    async function setupEthers() {
+      if (window.ethereum) {
+        // Connect to an Ethereum provider
+        const providerInstance = new ethers.BrowserProvider(window.ethereum);
+        setProvider(providerInstance);
+
+        // Get the signer (the user's account)
+        const signerInstance = await providerInstance.getSigner();
+        setSigner(signerInstance);
+
+        // The address of your smart contract
+        const contractAddress = "0x610178dA211FEF7D417bC0e6FeD39F05609AD788";
+
+        // The ABI (Application Binary Interface) of your smart contract
+        const contractABI = RainshowerContract.abi;
+
+        // Create a contract instance
+        const contractInstance = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signerInstance
+        );
+        setContract(contractInstance);
+      } else {
+        console.error("Ethereum provider not found");
+      }
+    }
+
+    setupEthers();
+  }, []);
+
   return (
     <div className="min-h-screen bg-gray-100">
       <Navbar />
-      <MainContent />
+      {/* Pass the contract instance to the MainContent component */}
+      <MainContent contract={contract} />
     </div>
   );
 }
